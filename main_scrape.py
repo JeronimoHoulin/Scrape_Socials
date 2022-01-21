243729bef4b0f98df2e9c23a163faa8725d72490
JeronimoHoulin <79488175+JeronimoHoulin@users.noreply.github.com>
2022-01-21 12:46:47 -0300

    Update main_scrape.py
    Added new feature: Can look for contained text in the tweet, from the URL as before..

--- main_scrape.py
+++ main_scrape.py
@@ -5,7 +5,34 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-#Defining functions
+
+#Creating an instance of a webdriver
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+""" Variables """
+
+#Making the URLs for the get function
+#tweet_fields = "tweet.fields=created_at,source,text,public_metrics,author_id"
+#query = "%40solana%20and%20ido%20-is%3aretweet"
+#url1 = "https://twitter.com/search?query={}&{}".format(query, tweet_fields)
+#Or the url itself (for searched items)
+
+url1 = "https://twitter.com/search?q=%23ido%20AND%20%23solana&src=typed_query"
+url2 = "https://twitter.com/search?q=%23ido%20AND%20%23solana&src=typed_query&f=live"
+## Or search by Username
+url3 = 'https://twitter.com/google'
+
+#Set other parameters: 
+contains = ""
+
+#GENERATE THE REQUEST !
+driver.get(url3)
+time.sleep(5)
+
+
+""" Function """
+
 def get_tweet_data(card):
     #Elements
     #GET FULL XPATH AND FOLLOW THROUGH AFTER TAG "ARTICLE"
@@ -17,31 +44,22 @@ def get_tweet_data(card):
     likes = card.find_element_by_xpath('.//div[@data-testid="like"]').text
     full_txt = text+responding
     
-    #Sponsored tweets have no date
-    try: 
-        date = card.find_element_by_xpath('.//time').get_attribute('datetime')
-    except NoSuchElementException: 
-        return
-    
-    #Create a tuple for the tweet
-    tweet = (handle, full_txt, date)
-    
-    return tweet
+    #Only add tweets w/ contains variable
+    if contains in text:
+        
+        #Sponsored tweets have no date
+        try: 
+            date = card.find_element_by_xpath('.//time').get_attribute('datetime')
+        except NoSuchElementException: 
+            return
 
-#Creating an instance of a webdriver
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+        #Create a tuple for the tweet
+        tweet = (handle, full_txt, date)
 
-#Making the URLs for the get function
-#tweet_fields = "tweet.fields=created_at,source,text,public_metrics,author_id"
-#query = "%40solana%20and%20ido%20-is%3aretweet"
-#url1 = "https://twitter.com/search?query={}&{}".format(query, tweet_fields)
-url1 = "https://twitter.com/search?q=%23ido%20AND%20%23solana&src=typed_query"
-url3 = "https://twitter.com/search?q=%23ido%20AND%20%23solana&src=typed_query&f=live"
-## Or search by Username
-url2 = 'https://twitter.com/google'
-driver.get(url3)
-time.sleep(5)
+        return tweet
+    
+    
+""" Scrolling """
 
 #Create tweet ID's for already scraped tweets
 tweet_ids = set()
@@ -56,15 +74,19 @@ scrolling = True
 
 #Looping through tweets
 while scrolling:
+    #Define the tweet segment
     cards = driver.find_elements_by_css_selector("article")
-    for card in cards[-15:]: 
+    
+    for card in cards: 
         tweet = get_tweet_data(card)
+        
         if tweet:
             tweet_id = ''.join(tweet)
             if tweet_id not in tweet_ids:
+                
+                #Add id & data
                 tweet_ids.add(tweet_id)
                 tweet_data.append(tweet)
-                
                 
                 
     scrolling_attempt = 0
@@ -77,14 +99,15 @@ while scrolling:
         curr_pos = driver.execute_script("return document.body.scrollHeight")
         
         if last_pos == curr_pos: 
+            #If I scrolled and ended up in the same place //nothing loaded//
             scrolling_attempt +=1
             
             #End of scroll region
-            if scrolling_attempt >= 3: 
+            if scrolling_attempt >= 4: 
                 scrolling = False
                 break
             else:
-                print("Atempting to scroll again.")
+                print(f'{scrolling_attempt}^st Attempt to scroll. Break in 4th.')
                 time.sleep(5)
         else:
             last_pos = curr_pos
